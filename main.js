111a7685babecb81f3eb6a6055168aab945f1e87
sebastian guevara <sebastianguevara32569830@gmail.com>
2022-01-18 21:46:58 -0300

    terminado

---
+++ main.js
@@ -0,0 +1,15 @@
+const main = () => {
+  const cuadrado = document.querySelector("div.cuadrado");
+  let acc = 0;
+
+  document.addEventListener("keydown", function (event) {
+    if (event.which == 37) {
+      acc -= 1;
+    } else if (event.which == 39) {
+      acc += 1;
+    }
+    cuadrado.style.marginLeft = acc + "rem";
+  });
+};
+
+main();
